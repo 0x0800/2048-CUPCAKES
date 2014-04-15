e01ba455cf800e1557b1cfce9d2087540bf604cc
Alex Petrosh <petrosh@users.noreply.github.com>
2014-04-15 12:27:08 +0200

    Update local_storage_manager.js
    Changed local storage keys for same origin problem

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -19,8 +19,8 @@ window.fakeStorage = {
 };
 
 function LocalStorageManager() {
-  this.bestScoreKey     = "bestScore";
-  this.gameStateKey     = "gameState";
+  this.bestScoreKey     = "bestScoreCupcakes";
+  this.gameStateKey     = "gameStateCupcakes";
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
